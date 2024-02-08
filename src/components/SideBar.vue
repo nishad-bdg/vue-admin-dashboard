2cd477619afe0873588ffa90fbb9b9e7cbe0bfec
Nishad Islam <nishad.nik@gmail.com>
2024-02-08 19:31:27 +0600

    color var green and gray added to the base.css, unnecessary gap remove from main.css and Sidebar svg icons added

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,19 +1,53 @@
 <template>
   <section class="sidebar">
-    <div>
+    <div class="sidebar-container">
       <img src="../assets/icons/logo.webp" class="logo" />
-      <div>Item1</div>
+      <div class="sidebar-items">
+        <IconHome />
+        <i class="fa fa-home" aria-hidden="true"></i>
+        <span>Overview</span>
+      </div>
+      <div class="sidebar-items">
+        <IconCurrency />
+        <span>Plans & Billings</span>
+      </div>
     </div>
-    
-    
+
+    <div class="sidebar-items">
+      <IconLogout /> <span>Logout {{ isTrue }}</span>
+    </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import IconHome from '@/components/Icons/IconHome.vue'
+import IconCurrency from '@/components/Icons/IconCurrency.vue'
+import IconLogout from '@/components/Icons/IconLogout.vue'
+</script>
 
 <style scoped>
 img {
   width: 160px;
   height: 40px;
+}
+
+.sidebar-items {
+  display: flex;
+  align-items: center;
+  color: var(--theme-font-gray);
+  font-weight: 500;
+  font-size: 1rem;
+  cursor: pointer;
+  padding: 15px;
+}
+
+.sidebar-items span {
+  margin-left: 5px;
+}
+
+.sidebar-items:hover {
+  background: #e1f2ef;
+  border-radius: 8px;
+  color: var(--theme-font-green);
 }
 </style>
---
+++ src/components/icons/IconHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M3 9.19598L12 2.19598L21 9.19598V20.196C21 20.7264 20.7893 21.2351 20.4142 21.6102C20.0391 21.9853 19.5304 22.196 19 22.196H5C4.46957 22.196 3.96086 21.9853 3.58579 21.6102C3.21071 21.2351 3 20.7264 3 20.196V9.19598Z"
+      :stroke="fill"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M9 22.196V12.196H15V22.196"
+      :stroke="fill"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+<script setup>
+defineProps({
+  fill: {
+    type: String,
+    default: '#929FA6'
+  }
+})
+</script>
