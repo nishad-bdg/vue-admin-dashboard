ea9dc8bc6d4a65bbefdb3116ebf736d12250a06c
Nishad Islam <nishad.nik@gmail.com>
2024-02-09 09:53:40 +0600

    sidebar active class added

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,20 +2,25 @@
   <section class="sidebar">
     <div class="sidebar-container">
       <img src="../assets/icons/logo.webp" class="logo" />
-      <div class="sidebar-items">
-        <IconHome />
+      <div
+        :class="`sidebar-items ${activeMenu === 'overview' && 'active'}`"
+        @click="activeMenu = 'overview'"
+      >
+        <IconHome :fill="activeMenu === 'overview' ? activeFill : defaultFill" />
         <i class="fa fa-home" aria-hidden="true"></i>
+        <span class="icon-logout"></span>
         <span>Overview</span>
       </div>
-      <div class="sidebar-items">
-        <IconCurrency />
+      <div
+        :class="`sidebar-items ${activeMenu === 'plans' && 'active'}`"
+        @click="activeMenu = 'plans'"
+      >
+        <IconCurrency :fill="activeMenu === 'plans' ? activeFill : defaultFill" />
         <span>Plans & Billings</span>
       </div>
     </div>
 
-    <div class="sidebar-items">
-      <IconLogout /> <span>Logout</span>
-    </div>
+    <div class="sidebar-items"><IconLogout /> <span>Logout</span></div>
   </section>
 </template>
 
@@ -23,6 +28,11 @@
 import IconHome from '@/components/Icons/IconHome.vue'
 import IconCurrency from '@/components/Icons/IconCurrency.vue'
 import IconLogout from '@/components/Icons/IconLogout.vue'
+import { ref } from 'vue'
+
+const activeMenu = ref('plans')
+const defaultFill = ref('#929FA6')
+const activeFill = ref('#12A288')
 </script>
 
 <style scoped>
@@ -47,6 +57,12 @@ img {
 }
 
 .sidebar-items:hover {
+  background: var(--theme-background-green);
+  border-radius: 8px;
+  color: var(--theme-font-green);
+}
+
+.sidebar-items.active {
   background: var(--theme-background-green);
   border-radius: 8px;
   color: var(--theme-font-green);
